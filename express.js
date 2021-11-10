1bc7fab9e72ef64d601e63b38dfe75e85ef5f153
= <=>
2021-11-10 21:59:29 +0400

    static in express

--- express.js
+++ express.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+// setup static and middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../HTML AND CSS/assignment.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1> Oops Not Found </h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Alet!!! Visitor is Here");
+});
+
 //First Example with Express
+/*
 app.get("/", (req, res) => {
   res.status(200).send("<h1> Welcome Back</h1>");
 });
@@ -14,3 +32,4 @@ app.all("*", (req, res) => {
 app.listen(3000, () => {
   console.log("Visitor is here");
 });
+*/
